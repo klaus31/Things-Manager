9e35467aa293253dcc065d066680d40eafabb1cf
klaus31 <danieloltmanns@gmx.de>
2018-10-07 06:45:06 +0200

    Text gets unlimited placeholders

--- src/main/renderer/config/MultiLanguage.js
+++ src/main/renderer/config/MultiLanguage.js
@@ -1,19 +1,10 @@
 class MultiLanguage {
 
-  get(key, a, b, c, d) {
+  get(key) {
     if (!TEXTS[key]) throw key + ' does not exist';
     let result = TEXTS[key][APP.languageCode];
-    if (a) {
-      result = result.replace("{1}", a);
-    }
-    if (b) {
-      result = result.replace("{2}", b);
-    }
-    if (c) {
-      result = result.replace("{3}", c);
-    }
-    if (d) {
-      result = result.replace("{4}", d);
+    for (let i = 1; i < arguments.length; i++) {
+      result = result.replace('{' + i + '}', arguments[i]);
     }
     return result;
   }
