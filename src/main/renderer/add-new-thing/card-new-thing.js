cc037a479ec3b2a54d6f32ba4de9f3a7da586bd4
klaus31 <danieloltmanns@gmx.de>
2018-10-19 03:48:05 +0200

    Fix Layout issues

--- src/main/renderer/add-new-thing/card-new-thing.js
+++ src/main/renderer/add-new-thing/card-new-thing.js
@@ -9,7 +9,7 @@ Vue.component('card-new-thing', {
       return this.ml.get('qei4gNQ2aHQxzXIt');
     }
   },
-  template: '<table class="card card-new-thing" v-colors="value.colors()">' +
+  template: '<table class="card card-new-thing editable" v-colors="value.colors()">' +
     '<thead>' +
     '<tr>' +
     '<th colspan="4">' +
@@ -22,13 +22,13 @@ Vue.component('card-new-thing', {
 
     '<tr>' +
     '<td>{{value.category.propertyKey.name}}</td>' +
-    '<td colspan="3"><input type="text" v-model="value.thing.keyvalue"></td>' +
+    '<td colspan="3" class="input"><input type="text" v-model="value.thing.keyvalue"></td>' +
     '</tr>' +
 
     '<tr v-for="property in value.thing.properties">' +
-    '<td><input type="text" v-model="property.key.name"></td>' +
-    '<td><input :type="property.key.type" v-model="property.value"></td>' +
-    '<td><select v-model="property.key.type"><option v-for="option in value.datatypeOptions" :value="option.value">{{option.label}}</option></select></td>' +
+    '<td class="input"><input type="text" v-model="property.key.name"></td>' +
+    '<td class="input"><input :type="property.key.type" v-model="property.value"></td>' +
+    '<td class="input"><select v-model="property.key.type"><option v-for="option in value.datatypeOptions" :value="option.value">{{option.label}}</option></select></td>' +
     '<td class="input buttons">' +
     '<tm-button @click="value.cloneProperty(property)" icon="duplicate" :title="ml_cdZVHIoilBz1bJ4e"></tm-button>' +
     '<tm-button @click="value.deleteProperty(property)" icon="trash" :title="ml_qei4gNQ2aHQxzXIt"></tm-button>' +
