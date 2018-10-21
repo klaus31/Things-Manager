6fa68e91b1a666180b9b45657c6bd8615f24d5c6
klaus31 <danieloltmanns@gmx.de>
2018-10-21 11:58:49 +0200

    Enhance Geodata Input

--- src/main/renderer/shared/data/model/Geodata.js
+++ src/main/renderer/shared/data/model/Geodata.js
@@ -2,41 +2,110 @@ class Geodata {
   constructor(raw) {
     // 00°00'00,0"N+00°00'00,0"E°
     if (raw) {
-      let geos = raw.replace(/\.[\d]+/g, '').split(/[^\d]/).filter(e => e);
-      this._n1 = geos[0] || null;
-      this._n2 = geos[1] || null;
-      this._n3 = geos[2] || null;
-      this._e1 = geos[3] || null;
-      this._e2 = geos[4] || null;
-      this._e3 = geos[5] || null;
+      let geos = raw.replace(/\.[\d]+/g, '').split(/[^-\d]/).filter(e => e);
+      this._nDegrees = geos[0] || null;
+      this._nMinutes = geos[1] || null;
+      this._nSeconds = geos[2] || null;
+      this._eDegrees = geos[3] || null;
+      this._eMinutes = geos[4] || null;
+      this._eSeconds = geos[5] || null;
+      if(raw.contains('S')) this._nDegrees *= -1;
+      if(raw.contains('W')) this._eDegrees *= -1;
     } else {
-      this._n1 = null;
-      this._n2 = null;
-      this._n3 = null;
-      this._e1 = null;
-      this._e2 = null;
-      this._e3 = null;
+      this._nDegrees = null;
+      this._nMinutes = null;
+      this._nSeconds = null;
+      this._eDegrees = null;
+      this._eMinutes = null;
+      this._eSeconds = null;
     }
   }
 
+  get nDegrees() {
+    return this._nDegrees;
+  }
+
+  set nDegrees(nDegrees) {
+    this._nDegrees = nDegrees % 90;
+  }
+
+  get nMinutes() {
+    return this._nMinutes;
+  }
+
+  set nMinutes(nMinutes) {
+    this._nMinutes = nMinutes % 60;
+  }
+
+  get nSeconds() {
+    return this._nSeconds;
+  }
+
+  set nSeconds(nSeconds) {
+    this._nSeconds = nSeconds % 60;
+  }
+
+  get eDegrees() {
+    return this._eDegrees;
+  }
+
+  set eDegrees(eDegrees) {
+    this._eDegrees = eDegrees % 180;
+  }
+
+  get eMinutes() {
+    return this._eMinutes;
+  }
+
+  set eMinutes(eMinutes) {
+    this._eMinutes = eMinutes % 60;
+  }
+
+  get eSeconds() {
+    return this._eSeconds;
+  }
+
+  set eSeconds(eSeconds) {
+    this._eSeconds = eSeconds % 60;
+  }
+
+  getEastAsDecimal() {
+    return (Number(this._eDegrees) + Number(this._eMinutes) / 60 + Number(this._eSeconds) / (60 * 60)).toFixed(7);
+  }
+
+  getNorthAsDecimal() {
+    return (Number(this._nDegrees) + Number(this._nMinutes) / 60 + Number(this._nSeconds) / (60 * 60)).toFixed(7);
+  }
+
   isComplete() {
-    return this._n1 !== null &&
-      this._n2 !== null &&
-      this._n3 !== null &&
-      this._e1 !== null &&
-      this._e2 !== null &&
-      this._e3 !== null;
+    return this._nDegrees !== null &&
+      this._nMinutes !== null &&
+      this._nSeconds !== null &&
+      this._eDegrees !== null &&
+      this._eMinutes !== null &&
+      this._eSeconds !== null;
+  }
+
+  convertDMSToDD(degrees, minutes, seconds, direction) {
+    return degrees + minutes / 60 + seconds / (60 * 60);
   }
 
   toString() {
     if (this.isComplete()) {
-      return `${this._n1}°${this._n2}'${this._n3}.0"N+${this._e1}°${this._e2}'${this._e3}.0"E`;
+      let vDirection = this._nDegrees < 0 ? 'S' : 'N';
+      let vDegrees = this._nDegrees < 0 ? this._nDegrees * -1 : this._nDegrees;
+      let hDirection = this._eDegrees < 0 ? 'W' : 'E';
+      let hDegrees = this._eDegrees < 0 ? this._eDegrees * -1 : this._eDegrees;
+      return `${vDegrees}°${this._nMinutes}'${this._nSeconds}.0"${vDirection} ${hDegrees}°${this._eMinutes}'${this._eSeconds}.0"${hDirection}`;
     } else {
       return '';
     }
   }
 
   getAsLink() {
-    return 'https://www.google.de/maps/place/' + this.toString();
+    console.info('https://www.google.de/maps/place/' + this.toString());
+    console.log(this);
+    return this.isComplete() ? 'https://www.google.de/maps/place/' + this.toString() : null;
+    // 'https://www.google.de/maps/place/@' + this.getNorthAsDecimal() + ',' + this.getEastAsDecimal() + ',21z';
   }
 }
--- src/main/renderer/shared/html/tm-input-geodata.js
+++ src/main/renderer/shared/html/tm-input-geodata.js
@@ -1,21 +1,32 @@
 Vue.component('tm-input-geodata', {
   props: ['content', 'actions'],
+  data: function () {
+    return {
+      geodata: new Geodata(this.content)
+    }
+  },
   methods: {
-    onChange: function (event) {
+    onChange: function () {
       if (this.actions && this.actions.onChange) {
-        this.actions.onChange(event.currentTarget.value);
+        this.actions.onChange(this.geodata.toString());
       }
     },
-    onDone: function (event) {
+    onDone: function () {
       if (this.actions && this.actions.onDone) {
-        this.actions.onDone(event.currentTarget.value);
+        this.actions.onDone(this.geodata.toString());
       }
     }
   },
-  template: '<span>' +
-    '<input type="text" v-autofocus v-autoselect :value="content" @input="onChange" @blur="onDone" @keydown.enter="onDone">' +
-    // TODO Multilanguage 'Example'
-    '<span>44°01\'01.0"N+28°38\'06.2"E</span>' +
+  template: '<span class="input-geodata">' +
+    // 00°00'00,0"N+00°00'00,0"E°
+    'N:&nbsp;<input type="number" v-autofocus v-autoselect v-model="geodata.nDegrees" min="-89" max="89" @keydown.enter= "onDone" @input="onChange">°&nbsp;&nbsp;&nbsp;' +
+    '<input type="number" v-model="geodata.nMinutes" min="0" max="59" @keydown.enter= "onDone" @input="onChange">\'&nbsp;&nbsp;&nbsp;' +
+    '<input type="number" v-model="geodata.nSeconds" min="0" max="59" @keydown.enter= "onDone" @input="onChange">.0"' +
+    '<br>' +
+    'E:&nbsp;<input type="number" v-model="geodata.eDegrees" min="-179" max="179" @keydown.enter= "onDone" @input="onChange">°&nbsp;&nbsp;&nbsp;' +
+    '<input type="number" v-model="geodata.eMinutes" min="0" max="59" @keydown.enter= "onDone" @input="onChange">\'&nbsp;&nbsp;&nbsp;' +
+    '<input type="number" v-model="geodata.eSeconds" min="0" max="59" @keydown.enter= "onDone" @input="onChange">.0"' +
+    '<br>' +
+    '<tm-button icon="ok" @click="onDone"></tm-button>' +
     '</span>'
-
 });
